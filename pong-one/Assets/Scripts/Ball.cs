fc03d1a72e333355fae5b4f8bccdbe120a5970d2
Lin <100497279+Collinb190@users.noreply.github.com>
2024-07-10 17:19:37 -0500

    why ui

--- pong-one/Assets/Scripts/Ball.cs
+++ pong-one/Assets/Scripts/Ball.cs
@@ -1,18 +1,70 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public float startSpeed = 400f;
+    public float increaseSpeed = 100f;
+    public float topSpeed = 600f;
+
+    private Rigidbody2D rb;
+    private Vector2 startingLaunch;
+    private Vector2 startingPoint;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        startingPoint = rb.position;
+        Launch();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Launch()
     {
-        
+        float randomDirectionY = Random.Range(-1, 2);
+        float randomDirectionX = Random.Range(0, 2) * 2 - 1;
+        startingLaunch = new Vector2(-1, randomDirectionY).normalized;
+        rb.velocity = startingLaunch * startSpeed;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Increase ball speed
+        if (rb.velocity.magnitude < topSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * (rb.velocity.magnitude + increaseSpeed);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Paddle"))
+        {
+            Debug.Log("hit paddle");
+            Debug.Log(rb.velocity.magnitude);
+            Vector2 sendBack = new Vector2(-rb.velocity.x, Random.Range(-90, 91)).normalized;
+            rb.velocity = sendBack * rb.velocity.magnitude;
+        }
+        else if (collision.CompareTag("Boundry"))
+        {
+            Debug.Log("hit boundry");
+            Debug.Log(rb.velocity.magnitude);
+            Vector2 sendBack = new Vector2(rb.velocity.x, -rb.velocity.y).normalized;
+            rb.velocity = sendBack * rb.velocity.magnitude;
+        }
+        else if (collision.CompareTag("Goal"))
+        {
+            Debug.Log("goal scored");
+            Debug.Log(rb.velocity.magnitude);
+            Reset();
+        }
+    }
+
+    void Reset()
+    {
+        rb.position = startingPoint;
+        startSpeed = 350f;
+        Launch();
     }
 }
+
--- pong-one/Assets/Scripts/Player.cs
+++ pong-one/Assets/Scripts/Player.cs
@@ -6,16 +6,29 @@ public class Player : MonoBehaviour
 {
     public KeyCode upKey;
     public KeyCode downKey;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float paddleSpeed = 400f;
+    public float verticalInput;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        MovePaddle();
+    }
+
+    void MovePaddle()
+    {
+        verticalInput = 0;
+
+        if (Input.GetKey(upKey))
+        {
+            verticalInput = 1;
+        }
+        else if (Input.GetKey(downKey))
+        {
+            verticalInput = -1;
+        }
+
+        transform.Translate(new Vector2(0, verticalInput * paddleSpeed * Time.deltaTime));
     }
 }
+
